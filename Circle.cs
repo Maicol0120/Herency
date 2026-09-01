c70b19104fadfd328ba23b9aec572297a23fde2a
MAICOL VILLA <maicolestivilla2003@outlook.es>
2026-08-31 22:31:58 -1200

    New Items

---
+++ Circle.cs
@@ -0,0 +1,12 @@
+﻿namespace GeometricFigures.Backend;
+
+public sealed class Circle : GeometricFigure
+{
+    public Circle(string name, double r) : base(name)
+    {
+        r = ValidatePositive (r, nameof(r));)
+    }
+
+    public override double GetArea() => Math.PI * R * R;
+    public override double GetPerimeter() => 2 * Math.PI * R; 
+}
